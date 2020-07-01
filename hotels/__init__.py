9cd840d6964bb85dea6adb293117a0e5e04ed1b8
navdhaagarwal <navdha16250@iiitd.ac.in>
2020-07-01 17:46:50 +0530

    cleanup

--- hotels/__init__.py
+++ hotels/__init__.py
@@ -2,9 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from hotels.routes.routes import mod
-
-app.register_blueprint(routes.routes.mod)
+import hotels.routes
 
 if __name__ == '__main__':
     app.run(debug=True)
--- hotels/routes.py
+++ hotels/routes.py
@@ -1,20 +1,18 @@
-from flask import Blueprint, session, render_template, request, redirect, url_for, abort, jsonify
+from flask import session, render_template, request, redirect, url_for, abort, jsonify
+from flask import Flask
 from flask_session import Session
-from flask_cors import CORS
+from hotels import app
+
 
 from .getHotelsData import get_goibibo_data
 
-from .getHotelsData import get_goibibo_data
 
-mod = Blueprint('main', __name__, template_folder='templates', static_folder='static')
-CORS(mod)
-
-@mod.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
-    return redirect(url_for("main.login"))
+    return redirect(url_for("login"))
 
 
-@mod.route("/register", methods=["GET", "POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         name = request.form.get("RegisterFullName")
@@ -32,7 +30,7 @@ def register():
     return render_template("register.html")
 
 
-@mod.route('/login', methods=["GET", "POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         email = request.form.get("LoginInputEmail")
@@ -45,7 +43,7 @@ def login():
     return render_template("login.html")
 
 
-@mod.route('/testapi/next=<next>', methods=['GET'])
+@app.route('/testapi/next=<next>', methods=['GET'])
 def api(next):
     params = (
         ('s', 'popularity^'),
@@ -61,4 +59,3 @@ def api(next):
     else:
         return jsonify({'d': data['city_meta_info']['ct'], 'next': data['next']}), 200
         
-
